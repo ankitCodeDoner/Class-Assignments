e5d2d009c276772ad5ab2ef5a3c93c9d4650c4de
Vijay <vv5063565@gmail.com>
2026-06-24 15:40:53 +0530

    Add files via upload

---
+++ Vijay/31Example.js
@@ -0,0 +1,264 @@
+// 1st example
+// let age = parseInt(prompt("Enter your age:"));
+// document.write("You are " + age + " years old.");
+
+// 2nd example
+// let price1 = parseFloat(prompt("Enter price of first item:"));
+// let price2 = parseFloat(prompt("Enter price of second item:"));
+
+// let total = price1 + price2;
+
+// document.write("Total price: " + total);
+
+// 3rd example
+// let employed = prompt("Are you currently employed? (true/false)") === "true";
+
+// document.write("Employment status: " + employed);
+
+// 4th example
+// let productName = prompt("Enter product name:");
+// let price = parseFloat(prompt("Enter product price:"));
+// let inStock = prompt("Is the product in stock? (true/false)") === "true";
+
+// document.write(`Product: ${productName}<br> Price: ${price}<br> In Stock: ${inStock ? "True" : "False"}`);
+
+// 5th example
+// let movie = prompt("Enter your favorite movie:")
+// document.write("Your favorite movie is " + movie);
+
+// 6th example
+// let birthYear = parseInt(prompt("Enter your birth year:"));
+// let age = 2025 - birthYear;
+// document.write("You are " + age + " years old");
+
+// 7th example
+// let completed = prompt("Have you completed the assignment? (true/false)") === "true";
+// document.write("Assignment completed: " + completed);
+
+// 8th example
+// let name = prompt("Enter student name:");
+// let rollNo = parseInt(prompt("Enter roll number:"));
+// let grade = prompt("Enter grade:");
+
+// document.write("Name: " + name + " <br> Roll No: " + rollNo + "<br> Grade: " + grade);
+
+// 9th example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num >= 0) {
+//     document.write("Positive Number");
+// } else {
+//     document.write("Negative Number");
+// }
+
+// 10th example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num % 2 === 0) {
+//     document.write("Even Number");
+// } else {
+//     document.write("Odd Number");
+// }
+
+// 11th example
+// let age = parseInt(prompt("Enter your age:"));
+
+// if (age >= 18) {
+//     document.write("Eligible to vote");
+// } else {
+//     document.write("Not eligible");
+// }
+
+// 12th example
+// let num1 = parseInt(prompt("Enter first number:"));
+// let num2 = parseInt(prompt("Enter second number:"));
+
+// if (num1 > num2) {
+//     document.write(num1 + " is greater");
+// } else if (num2 > num1) {
+//     document.write(num2 + " is greater");
+// } else {
+//     document.write("Both numbers are equal");
+// }
+
+// 13th example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num % 5 === 0) {
+//     document.write("Number is divisible by 5");
+// } else {
+//     document.write("Number is not divisible by 5");
+// }
+
+// 14th example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num % 3 === 0) {
+//     document.write("Number is a multiple of 3");
+// } else {
+//     document.write("Number is not a multiple of 3");
+// }
+
+// 15th example
+// let str = prompt("Enter a string:");
+
+// if (str === "") {
+//     document.write("String is empty");
+// } else {
+//     document.write("String is not empty");
+// }
+
+// 16th example
+// let password = prompt("Enter password:");
+
+// if (password === "admin123") {
+//     document.write("Password is correct");
+// } else {
+//     document.write("Incorrect password");
+// }
+
+// 17th example
+// let marks = parseInt(prompt("Enter marks:"));
+
+// if (marks >= 40) {
+//     document.write("Student Passed");
+// } else {
+//     document.write("Student Failed");
+// }
+
+// 18th example
+// let amount = parseInt(prompt("Enter purchase amount:"));
+
+// if (amount > 500) {
+//     document.write("Eligible for discount");
+// } else {
+//     document.write("Not eligible for discount");
+// }
+
+// 19th example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num > 1000) {
+//     document.write("Number is greater than 1000");
+// } else {
+//     document.write("Number is not greater than 1000");
+// }
+
+// 20th example
+// let age = parseInt(prompt("Enter your age:"));
+
+// if (age >= 18) {
+//     document.write("Adult");
+// } else {
+//     document.write("Minor");
+// }
+
+// 21st example
+// let weight = parseFloat(prompt("Enter parcel weight (kg):"));
+
+// if (weight > 10) {
+//     document.write("Parcel is heavy");
+// } else {
+//     document.write("Parcel is not heavy");
+// }
+
+// 22nd example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num <= 9) {
+//     document.write("Single-digit number");
+// } else {
+//     document.write("Not a single-digit number");
+// }
+
+// 23rd example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num % 10 === 5) {
+//     document.write("Number ends in 5");
+// } else {
+//     document.write("Number does not end in 5");
+// }
+
+// 24th example
+// let num1 = parseInt(prompt("Enter first number:"));
+// let num2 = parseInt(prompt("Enter second number:"));
+
+// let sum = num1 + num2;
+
+// if (sum > 100) {
+//     document.write(sum + " is greater than 100");
+// } else {
+//     document.write(sum + " is not greater than 100");
+// }
+
+// 25th example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num == 2 || num == 3 || num == 5 || num == 7 || num == 11) {
+//     document.write("Prime Number");
+// } else {
+//     document.write("Not a Prime Number");
+// }
+
+// 26th example
+// let num1 = parseInt(prompt("Enter first number:"));
+// let num2 = parseInt(prompt("Enter second number:"));
+// let num3 = parseInt(prompt("Enter third number:"));
+
+// if (num1 >= num2 && num1 >= num3) {
+//     document.write(num1 + " is the largest number");
+// } else if (num2 >= num1 && num2 >= num3) {
+//     document.write(num2 + " is the largest number");
+// } else {
+//     document.write(num3 + " is the largest number");
+// }
+
+// 27th example
+// let year = parseInt(prompt("Enter a year:"));
+
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     document.write("Leap Year");
+// } else {
+//     document.write("Not a Leap Year");
+// }
+
+// 28th example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num >= 100 && num <= 999) {
+//     document.write("It is a 3-digit number");
+// } else {
+//     document.write("It is not a 3-digit number");
+// }
+
+// 29th example
+// let temp = parseInt(prompt("Enter temperature:"));
+
+// if (temp >= 30) {
+//     document.write("Hot");
+// } else if (temp >= 20 && temp <= 29) {
+//     document.write("Moderate");
+// } else {
+//     document.write("Cold");
+// }
+
+// 30th example
+// let salary = parseFloat(prompt("Enter salary:"));
+
+// if (salary > 50000) {
+//     document.write("Bonus: 20%");
+// } else if (salary >= 30000 && salary <= 50000) {
+//     document.write("Bonus: 10%");
+// } else {
+//     document.write("Bonus: 5%");
+// }
+
+// 31st example
+// let num = parseInt(prompt("Enter a number:"));
+
+// if (num >= 10 && num <= 50) {
+//     document.write("Number lies between 10 and 50");
+// } else {
+//     document.write("Number does not lie between 10 and 50");
+// }
